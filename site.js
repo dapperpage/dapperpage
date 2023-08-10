bc76410272b1b8405ad151167cdefb7d6e25365f
dapperpage <mat@dapperpage.com>
2023-08-09 23:10:39 -0700

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// Set a cookie
+Cookies.set("cookieName", "cookieValue", { expires: 365 });
+
+// Get a cookie
+Cookies.get("cookieName");
+
+// Check a cookie
+if (Cookies.get("cookieName") == "cookieValue") {
+   // do something
+}
+
+// Delete a cookie
+Cookies.remove('cookieName');
+
+// Loop through each item with a class
+$(".cms_item").each(function (index) {
+  console.log( $(this).index() );
+});

910cb7cb86eb150c9ce3b216a74bff15a472e393
dapperpage <mat@dapperpage.com>
2023-08-13 22:33:52 -0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -26,7 +26,7 @@ function scrollR(x) {
     if (isMobile) {
       // Create an Intersection Observer instance
       const width = window.innerWidth;
-      const rm = 200 - width;
+      const rm = 300 - width;
       const options = {
               root: null,
               threshold: 0,

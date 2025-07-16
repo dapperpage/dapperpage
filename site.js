21c112df666d0957049f54b8771eb17132eb3676
dapperpage <mat@dapperpage.com>
2025-07-16 15:29:24 -0700

    removed favorites redirect from conference. changed 'clear favorites' function to only clear the local storage for favorite items

--- site.js
+++ site.js
@@ -1,34 +1,40 @@
 // Check for favorites  
- $(".track-item").each(function (index) {
-  let itemName = $(this).find('.track-title').text();
-  let storageName = localStorage.getItem(itemName);
-  if(storageName == "saved") {
-  $(this).addClass('is-favorite');
-  }
+$(".track-item").each(function (index) {
+   let itemName = $(this).find('.track-title').text();
+   let storageName = localStorage.getItem(itemName);
+   if (storageName == "isFavorite") {
+      $(this).addClass('is-favorite');
+   }
 });
-  
+
 
 // Add or remove favorites
-$('.favorite').on('click', function() {
-  let savedParent = $(this).closest('.track-item');
-  let parentIndex = savedParent.index();
-  let title = savedParent.find('.track-title').text();
-  savedParent.toggleClass('is-favorite');
-  savedParent.closest('.track-list-wrapper').siblings('.track-list-wrapper').find('.track-item').eq(parentIndex).toggleClass('is-favorite');
-  if(savedParent.hasClass('is-favorite')) {
-    let storageName = title;
-    localStorage.setItem(storageName, "saved");
-  } else {
-    let storageName = title;
-    localStorage.removeItem(storageName);
-  }
+$('.favorite').on('click', function () {
+   let savedParent = $(this).closest('.track-item');
+   let parentIndex = savedParent.index();
+   let title = savedParent.find('.track-title').text();
+   savedParent.toggleClass('is-favorite');
+   savedParent.closest('.track-list-wrapper').siblings('.track-list-wrapper').find('.track-item').eq(parentIndex).toggleClass('is-favorite');
+   if (savedParent.hasClass('is-favorite')) {
+      let storageName = title;
+      localStorage.setItem(storageName, "isFavorite");
+   } else {
+      let storageName = title;
+      localStorage.removeItem(storageName);
+   }
 });
-  
+
 // Clear favorites
-$('.reset').on('click', function() {
-  $('.track-item').each(function (index) {
-  $(this).removeClass('is-favorite');
-  });
-  localStorage.clear();
-  setTimeout('window.location.href = "https://www.dapperpage.com/conference-interaction/listen-conference"', 3000);
+$('.reset').on('click', function () {
+   $('.track-item').each(function (index) {
+      $(this).removeClass('is-favorite');
+      let title = $(this).find('.track-title').text();
+      let storageName = title;
+      if (localStorage.getItem(storageName) === "isFavorite") {
+         localStorage.removeItem(storageName);
+      }
+   });
+   //   localStorage.clear(); // I don't want to clear all the local storage, just the favorites
+   //   setTimeout('window.location.href = "https://www.dapperpage.com/conference-interaction/listen-conference"', 3000); //this line is for redirecting after clearing favorites from conference interaction page
 });
+

d67eb3199fdc29e879e242e9cbcb138257d27202
dapperpage <mat@dapperpage.com>
2025-07-16 15:32:53 -0700

    updated line-clamp

--- favorites.js
+++ favorites.js
@@ -2,7 +2,7 @@
   let itemName = $(this).find('.track-title').text();
   let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();
   let storageName = localStorage.getItem(itemName);
-  if(storageName == "saved") {
+  if(storageName == "isFavorite") {
    console.log(itemURL);
   }
 });

0e9c590ad21a50c64f9a0cfe61a1daa3c874a0fa
dapperpage <mat@dapperpage.com>
2025-07-16 15:50:55 -0700

    v.1.0.0 testing to see if version number will show up

--- favorites.js
+++ favorites.js
@@ -1,3 +1,5 @@
+ //v.1.0.0
+ 
  $(".is-favorite").each(function (index) {
   let itemName = $(this).find('.track-title').text();
   let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();

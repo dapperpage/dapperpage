3fc4e819b9b91461fdfb46cb38f32ceca6b1988e
dapperpage <mat@dapperpage.com>
2025-07-23 01:55:00 -0700

    last one, I promise. try to remove 'is-favorite' class to see if that fixes it.

--- site.js
+++ site.js
@@ -67,6 +67,7 @@ function getStorage() {
             item.classList.add('is-favorite');
             favIcon.src = "https://cdn.prod.website-files.com/640788df21cddc9b2f29bc16/68783b6bd8c41c933269ad27_heart-solid%20(1).svg";
          } else {
+            item.classList.remove('is-favorite');
             favIcon.src = "https://cdn.prod.website-files.com/640788df21cddc9b2f29bc16/68783b97f7f86dbb71aeef0c_heart-regular%20(1).svg";
          }
          if (parsedItem.order) {

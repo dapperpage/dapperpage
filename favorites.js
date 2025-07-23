a9a8864dadd3fcc3ca53bb5178071b9bdbf76f9d
dapperpage <mat@dapperpage.com>
2025-07-23 00:57:40 -0700

    updating favorites.js to see if favorites will populate

--- favorites.js
+++ favorites.js
@@ -1,41 +1,43 @@
- //v.1.0.0
- 
- $(".is-favorite").each(function (index) {
-  let itemName = $(this).find('.track-title').text();
-  let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();
-  let storageName = localStorage.getItem(itemName);
-  if(storageName == "isFavorite") {
-   console.log(itemURL);
-  }
-});
+// //get favorites from local storage
+//  $(".is-favorite").each(function (index) {
+//   let itemName = $(this).find('.track-title').text();
+//   let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();
+//   let storageName = localStorage.getItem(itemName);
+//   if(storageName == "isFavorite") {
+//    console.log(itemURL);
+//   }
+// });
 
 
-//from Favorites Conference page
-$('.favorite-item-container').on('click', function() {
-  $(this).toggleClass('is-favorite');
-});
+// //add or remove favorites
+// $('.favorite-item-container').on('click', function() {
+//   $(this).toggleClass('is-favorite');
+// });
 
+// //print favorites
+// const fav = document.getElementById('favorites');
+// fav.value = "<table style='boarder: 5px solid black;' width=400px>"
+//  $(".is-favorite").each(function (index) {
+//   let itemName = $(this).find('.track-title').text();
+//   let itemImg = $(this).find('.fav-img');
+//   let itemID = $(this).find('.pub-id').text();
+//   let itemPrice = $(this).find('.fav-price').text();
+//   let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();
+//   let storageName = localStorage.getItem(itemName);
+//   if(storageName == "isFavorite") {
+//     fav.value += "<tr><td style='border: 1px solid black; padding: 1rem; color: #333;'>" + itemID + "</td><td style='border: 1px solid black; padding: 1rem; color: #333;'><a href='" + itemURL + "'>" + itemName + "</a></td><td style='border: 1px solid black; padding: 1rem; color: #333;'>" + itemPrice + "</td></tr>";
+//   }
+// });
+// fav.value += "</table>"
 
-const fav = document.getElementById('favorites');
-fav.value = "<table style='boarder: 5px solid black;' width=400px>"
- $(".is-favorite").each(function (index) {
-  let itemName = $(this).find('.track-title').text();
-  let itemImg = $(this).find('.fav-img');
-  let itemID = $(this).find('.pub-id').text();
-  let itemPrice = $(this).find('.fav-price').text();
-  let itemURL = "https://www.dapperpage.com/dapper-page-titles/" + $(this).find('.url-slug').text();
-  let storageName = localStorage.getItem(itemName);
-  if(storageName == "saved") {
-    fav.value += "<tr><td style='border: 1px solid black; padding: 1rem; color: #333;'>" + itemID + "</td><td style='border: 1px solid black; padding: 1rem; color: #333;'><a href='" + itemURL + "'>" + itemName + "</a></td><td style='border: 1px solid black; padding: 1rem; color: #333;'>" + itemPrice + "</td></tr>";
-  }
-});
-fav.value += "</table>"
+// //reset on email submit
+// $('#fav-email').on('click', function() {
+//   $('#reset').click();
+//   $('.back-to-list-button').click();
+// });
 
-//reset on email submit
-$('#fav-email').on('click', function() {
-  $('#reset').click();
-  $('.back-to-list-button').click();
-});
+// //print page
+// $("#fav-print").click(function() { window.print(); });
 
-//print page
-$("#fav-print").click(function() { window.print(); });
+getStorage();
+

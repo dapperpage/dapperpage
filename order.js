38a1f8d28fd03c9d3605d6a5f072601fa400c244
dapperpage <mat@dapperpage.com>
2025-07-24 10:09:10 -0700

    added print button function to order page

--- order.js
+++ order.js
@@ -1,3 +1,10 @@
 getStorage(undefined, undefined, trashIcon, listIconLight);
 handleFavButtons();
 handleOrderButtons(trashIcon, listIconLight);
+
+// Print Order
+const printOrderBtn = document.getElementById("printOrder");
+
+printOrderBtn.addEventListener("click", () => {
+    window.print();
+});

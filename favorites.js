22fb429439bd8c50b8d35e5fcaf51efa2b09db7d
dapperpage <mat@dapperpage.com>
2025-07-29 21:38:06 -0700

    added event listener for clear favorites button

--- favorites.js
+++ favorites.js
@@ -1,3 +1,7 @@
 getStorage({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
 handleFavButtons({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
 handleOrderButtons({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
+
+// Clear all favorites
+const clearFavsBtn = document.getElementById("clearFavs");
+clearFavsBtn.addEventListener("click", () => {clearFavorites();});

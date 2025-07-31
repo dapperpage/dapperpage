9d0474e16599843c0df2dba3363bdbb55402e87d
dapperpage <mat@dapperpage.com>
2025-07-31 00:07:52 -0700

    same empty list functionality as the purchase order page for favorites

--- favorites.js
+++ favorites.js
@@ -1,22 +1,28 @@
-getStorage({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
-handleFavButtons({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
-handleOrderButtons({favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill});
+getStorage({ favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill });
+handleFavButtons({ favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill });
+handleOrderButtons({ favIconSrc: trashIcon, unfavIconSrc: heartIconNoFill });
 animateDropdown();
 
 // Clear all favorites
 const clearFavsBtn = document.getElementById("clearFavs");
 function handleClearFavorites() {
-    clearFavsBtn.addEventListener("click", () => {clearFavorites();});
-    displayClearFavoritesButton();
+    clearFavsBtn.addEventListener("click", () => {
+        clearFavorites();
+        emptyFavsDisplay();
+    });
 }
 
-function displayClearFavoritesButton() {
+function emptyFavsDisplay() {
     const favorites = document.querySelectorAll('.is-favorite');
+    const emptyList = document.getElementById("emptyList");
     if (favorites.length === 0) {
         clearFavsBtn.style.display = "none";
+        emptyList.style.display = "block"; // Show the empty list message
     } else {
         clearFavsBtn.style.display = "block";
+        emptyList.style.display = "none"; // Hide the empty list message
     }
 }
 
 handleClearFavorites();
+emptyFavsDisplay();

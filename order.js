4f9030bc321a78b4dbff4be94cf3f402be70f4c7
dapperpage <mat@dapperpage.com>
2025-08-12 16:15:50 -0700

    added function to add school info to print page on order page

--- order.js
+++ order.js
@@ -68,3 +68,55 @@ function emptyListDisplay() {
 
 handleClearOrders();
 emptyListDisplay();
+
+// Enter address information into print page
+function schoolInfo() {
+    const name = document.getElementById("name");
+    const school = document.getElementById("school");
+    const address1 = document.getElementById("address1");
+    const address2 = document.getElementById("address2");
+    const city = document.getElementById("city");
+    const state = document.getElementById("state");
+    const zip = document.getElementById("zip");
+    const phone = document.getElementById("phone");
+    const email = document.getElementById("email");
+    const poName = document.getElementById("poName");
+    const poSchool = document.getElementById("poSchool");
+    const poAddress1 = document.getElementById("poAddress1");
+    const poAddress2 = document.getElementById("poAddress2");
+    const poCity = document.getElementById("poCity");
+    const poState = document.getElementById("poState");
+    const poZip = document.getElementById("poZip");
+    const poPhone = document.getElementById("poPhone");
+    const poEmail = document.getElementById("poEmail");
+
+    name.addEventListener("input", () => {
+        poName.innerHTML = name.value;
+    });
+    school.addEventListener("input", () => {
+        poSchool.innerHTML = school.value;
+    });
+    address1.addEventListener("input", () => {
+        poAddress1.innerHTML = address1.value;
+    });
+    address2.addEventListener("input", () => {
+        poAddress2.innerHTML = address2.value;
+    });
+    city.addEventListener("input", () => {
+        poCity.innerHTML = city.value;
+    });
+    state.addEventListener("input", () => {
+        poState.innerHTML = state.value;
+    });
+    zip.addEventListener("input", () => {
+        poZip.innerHTML = zip.value;
+    });
+    phone.addEventListener("input", () => {
+        poPhone.innerHTML = phone.value;
+    });
+    email.addEventListener("input", () => {
+        poEmail.innerHTML = email.value;
+    });
+}
+
+schoolInfo();

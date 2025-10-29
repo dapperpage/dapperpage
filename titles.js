3cb684da0de6f213e07bcc8386feb0e140963663
dapperpage <mat@dapperpage.com>
2025-10-29 16:58:27 -0700

    added generateTitle function to show the title in the H1 element

--- titles.js
+++ titles.js
@@ -2,11 +2,17 @@ const nouns = document.querySelector("#plural-noun");
 const verbing = document.querySelector("#verb-ending-in--ing");
 const subBtn1 = document.querySelector("#sub-btn-1");
 const modal = document.querySelector("#tt-modal");
+const title = document.querySelector("#tt-title");
 
 subBtn1.addEventListener("click", () => {
+    generateTitle();
     modal.classList.remove("hidden");
 });
 
 modal.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
+
+function generateTitle() {
+    title.textContent = `${verbing.value} ${nouns.value}`;
+}

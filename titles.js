e75e98e600b38c052c68e6a3c23c76d14b6ffabe
dapperpage <mat@dapperpage.com>
2025-10-29 11:13:32 -0700

    added variables and event listeners to open and close modal

--- titles.js
+++ titles.js
@@ -1,2 +1,12 @@
-console.log("hello");
-console.log("not you again");
+const nouns = document.querySelector("#plural-noun");
+const verbing = document.querySelector("#verb-ending-in--ing");
+const subBtn1 = document.querySelector("#sub-btn-1");
+const modal = document.querySelector("#tt-modal");
+
+subBtn1.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+modal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
